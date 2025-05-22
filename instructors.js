d90e5a44f54fb4361805a5027a2dfee2184ecd30
CodeSandbox <support@codesandbox.io>
2025-05-22 22:27:05 +0000

    modified:   instructors.html 	modified:   instructors.js

--- instructors.js
+++ instructors.js
@@ -22,5 +22,37 @@ const instructors = [
         specialties: ["Instrument Rating", "Flight Instructor", "Certified Flight Instructor Instrument"],
         bio: "Michael is an expert in instrument training and cross-country flights.\n\nHe focuses on building pilot confidence.",
         bookingLink: "https://calendly.com/michaelbrown/lesson"
+    },
+    {
+        name: "Oliver Emde - Flight Instructor",
+        credentials: "CFI",
+        photo: "assets/images/Instructors/Oliver Emde.jpg",
+        specialties: ["Private", "Single Engine Commercial"],
+        bio: "Coming from a family of pilots I've always known what I wanted to do with my life. I grew up in Maui, Hawaii, where I embarked on my aviation journey at the age of 17. Flying in Maui with some of the strongest crosswinds in the world I perfected my craft. Since then, I have relocated to California to further pursue my dream. Whether you're pursuing a career or learning for fun, I am eager to share my knowledge to help you become a safe and knowledgeable pilot!",
+        bookingLink: "https://oliveremde11.youcanbook.me/"
+    },
+    {
+        name: "Jesse Balasek - Flight Instructor",
+        credentials: "CFI, CFII",
+        photo: "assets/images/Instructors/Jesse Balasek.jpg",
+        specialties: ["Private", "Instrument", "Single Engine Commercial"],
+        bio: "If you love what you do, you'll never work a day in your life. Flying has been my true passion for as long as I can remember. Both of my fathers were Professional Pilots, and I've always dreamed of being as great as they were one day. Ive learned that being a great pilot starts with having a great instructor and learning that safety is the most important thing. I want to be there to help guide my students to be the safest and best version of themselves as Pilots. Giddy Up",
+        bookingLink: "https://jessebalasek.youcanbook.me/"
+    },
+    {
+        name: "Daniel Durrand - Flight Instructor",
+        credentials: "CFI, CFII",
+        photo: "assets/images/Instructors/Daniel Durrand.jpg",
+        specialties: ["Private", "Instrument", "Single Engine Commercial"],
+        bio: "Let's get you flying! When I started flight training, I was a weekend warrior with an insurance day job, but what started as a fun and challenging hobby became an exciting passion and rewarding career. Regardless of your goals, flying offers you the opportunity to tackle new challenges, embrace adventure, and (literally) see the world from a new perspective. Whether you're looking to get in the plane just for fun, or to eventually fly professionally, I will help you become a safe and capable pilot. I look forward to sharing the cockpit with you!",
+        bookingLink: "https://cfidurrand.youcanbook.me/"
+    },
+    {
+        name: "Mason Liu - Flight Instructor",
+        credentials: "CFI, CFII",
+        photo: "assets/images/Instructors/Mason Liu1.jpg",
+        specialties: ["Private", "Instrument", "Single Engine Commercial"],
+        bio: "Flying has always felt like an impossible dream to me. Yet, after earning my PhD in physics and pursuing a career in finance, I decided to start flight training. I discovered where my true passion lies. My academic background and professional experience allow me to connect with both younger learners and those with rich life experience. I would be thrilled to help you achieve your goals and become a safe, capable pilot. Let's take to the skies!",
+        bookingLink: "https://masonliu.youcanbook.me/"
     }
 ];
