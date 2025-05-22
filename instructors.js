e5b98c13369ba0c71bf776ac3030bff93b5f6b9b
CodeSandbox <support@codesandbox.io>
2025-05-22 22:14:31 +0000

    modified:   instructors.html 	modified:   instructors.js 	new file:   instructors_render.js

--- instructors.js
+++ instructors.js
@@ -1,11 +1,11 @@
 const instructors = [
     {
         name: "Michael Mourer",
-        credentials: "CFI, ATP",
+        credentials: "CFI, CFII, MEI, ATP",
         photo: "assets/images/Instructors/Michael.jpg",
         specialties: ["Private Pilot", "Instrument Rating", "Commercial Pilot"],
         bio: "John has over 15 years of experience as a flight instructor.\n\nHe is passionate about teaching and safety.",
-        bookingLink: "https://calendly.com/johndoe/lesson"
+        bookingLink: "https://newvisionaviation.youcanbook.me/"
     },
     {
         name: "Josh Friedman",
