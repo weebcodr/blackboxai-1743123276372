53bc06d014f63a9b50548afaafdc2d3b3bae93f3
weebcodr <joeldavilaromero18@gmail.com>
2025-11-15 15:48:00 -0800

    modified:   instructors.html 	modified:   instructors.js

--- instructors.js
+++ instructors.js
@@ -3,7 +3,7 @@ const instructors = [
         name: "Michael Mourer - President - Chief Pilot",
         credentials: "CFI, CFII, MEI, ATP",
         photo: "assets/images/Instructors/Michael.jpg",
-        specialties: ["Private", "Instrument", "Single Engine Commercial", "Multi Engine Commercial", "Initial CFI", "Instructor Instrument", "Instructor Multi Engine", "FR", "IPC", "HP"],
+        specialties: ["Private", "Instrument", "Single Engine Commercial", "Multi Engine Commercial", "Initial CFI", "Instructor Instrument", "Instructor Multi Engine", "Flight Review", "Instrument Proficiency Check", "High Performance", "Complex"],
         approvedAircraft: ["C172", "C172RG", "C182FG", "C182RG", "C182 G1000", "PA28", "PA28R", "DA40", "BE33", "BE76"], 
         bio: "I started New Vision Aviation with the idea that a group of likeminded instructors would work together to maintain a high level of training in a cooperative environment to benefit the student. My colleagues and I ensure this happens by continuing to expand our education and knowledge in the industry. I also promote and support a custom tailored training program to meet the needs of the individual student.",
         bookingLink: "https://newvisionaviation.youcanbook.me/"
@@ -12,10 +12,10 @@ const instructors = [
         name: "Josh Friedman - Flight Instructor",
         credentials: "CFI, CFII",
         photo: "assets/images/Instructors/Josh Friedman2.jpg",
-        specialties: ["Private", "Instrument", "Single Engine Commercial", "FR", "IPC"],
+        specialties: ["Private", "Instrument", "Single Engine Commercial", "Flight Review", "Instrument Proficiency Check"],
         approvedAircraft: ["C172" ,"PA28", "DA40"], 
         bio: "Let's go flying! Since I was a kid I dreamed of becoming a pilot. Unfortunately at the time I didn't know any and it seemed impossible. All of that has changed! I'm now a part of the aviation community. So whether you're interested in a new hobby and sailing seems a bit too slow, or you're pursuing a career path such as myself, we'll work together to make you a safe and competent pilot. There's no field like it, and the industry is growing fast. Now is the time!",
-        bookingLink: "https://joshua-friedman.youcanbook.me/"
+        bookingLink: "https://joshua-Friedman.youcanbook.me/"
     },
     
     {
@@ -32,16 +32,16 @@ const instructors = [
         name: "Daniel Durrand - Flight Instructor",
         credentials: "CFI, CFII",
         photo: "assets/images/Instructors/Daniel Durrand.jpg",
-        specialties: ["Private", "Instrument", "Single Engine Commercial", "FR", "IPC"],
+        specialties: ["Private", "Instrument", "Single Engine Commercial", "Flight Review", "Instrument Proficiency Check"],
         approvedAircraft: ["C172", "PA28"], 
-        bio: "Let's get you flying! When I started flight training, I was a weekend warrior with an insurance day job, but what started as a fun and challenging hobby became an exciting passion and rewarding career. Regardless of your goals, flying offers you the opportunity to tackle new challenges, embrace adventure, and (literally) see the world from a new perspective. Whether you're looking to get in the plane just for fun, or to eventually fly professionally, I will help you become a safe and capable pilot. I look forward to sharing the cockpit with you!",
+        bio: "Let's get you flying! When I started flight training, I was a weekend warrior with an insurance day job, but what started as a fun and challenging hobby became an exciting passion and rewarding career. Regardless of your goals, flying offers you the opportunity to tackle new challenges, embrace adventure, and (literally) see the world Flight Reviewom a new perspective. Whether you're looking to get in the plane just for fun, or to eventually fly professionally, I will help you become a safe and capable pilot. I look forward to sharing the cockpit with you!",
         bookingLink: "https://cfidurrand.youcanbook.me/"
     },
     {
         name: "Mason Liu - Flight Instructor",
         credentials: "CFI, CFII",
         photo: "assets/images/Instructors/Mason Liu1.jpg",
-        specialties: ["Private", "Instrument", "Single Engine Commercial", "FR", "IPC", "HP"],
+        specialties: ["Private", "Instrument", "Single Engine Commercial", "Flight Review", "Instrument Proficiency Check", "High Performance", "Complex"],
         approvedAircraft: ["C172", "C182FG", "PA28"], 
         bio: "Flying has always felt like an impossible dream to me. Yet, after earning my PhD in physics and pursuing a career in finance, I decided to start flight training. I discovered where my true passion lies. My academic background and professional experience allow me to connect with both younger learners and those with rich life experience. I would be thrilled to help you achieve your goals and become a safe, capable pilot. Let's take to the skies!",
         bookingLink: "https://masonliu.youcanbook.me/"
@@ -50,7 +50,7 @@ const instructors = [
         name: "Andrew Larson - Flight Instructor",
         credentials: "CFI, CFII",
         photo: "assets/images/Instructors/AndrewL.JPG",
-        specialties: ["Private", "Instrument", "Single Engine Commercial", "FR", "IPC"],
+        specialties: ["Private", "Instrument", "Single Engine Commercial", "Flight Review", "Instrument Proficiency Check"],
         approvedAircraft: ["C172"],
         bio: "Flying is a never ending journey of experiences and learning. I started my journey at 17 and continued onto Embry Riddle Aeronautical University where I grew my foundation and got a Bachelors in Aeronautical Science. My passion for aviation pushes me to keep learning and fine tune my skills to help train safe and confident pilots. Let’s hit the skies and help you achieve your aviation goals.",
         bookingLink: "https://andrewl17.youcanbook.me/"
