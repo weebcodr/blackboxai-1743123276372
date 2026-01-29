edbfd59b54895bfd9ed2b5d00993fce8ab8271ac
weebcodr <166180432+weebcodr@users.noreply.github.com>
2026-01-28 23:31:39 -0800

    added master class, and fixed minor issues

--- instructors.js
+++ instructors.js
@@ -8,26 +8,7 @@ const instructors = [
         bio: "I started New Vision Aviation with the idea that a group of likeminded instructors would work together to maintain a high level of training in a cooperative environment to benefit the student. My colleagues and I ensure this happens by continuing to expand our education and knowledge in the industry. I also promote and support a custom tailored training program to meet the needs of the individual student.",
         bookingLink: "https://newvisionaviation.youcanbook.me/"
     },
-    {
-        name: "Josh Friedman - Flight Instructor",
-        credentials: "CFI, CFII",
-        photo: "assets/images/Instructors/Josh Friedman2.jpg",
-        specialties: ["Private", "Instrument", "Single Engine Commercial", "Flight Review", "Instrument Proficiency Check"],
-        approvedAircraft: ["C172" ,"PA28", "DA40"], 
-        bio: "Let's go flying! Since I was a kid I dreamed of becoming a pilot. Unfortunately at the time I didn't know any and it seemed impossible. All of that has changed! I'm now a part of the aviation community. So whether you're interested in a new hobby and sailing seems a bit too slow, or you're pursuing a career path such as myself, we'll work together to make you a safe and competent pilot. There's no field like it, and the industry is growing fast. Now is the time!",
-        bookingLink: "https://joshua-Friedman.youcanbook.me/"
-    },
-    
-    {
-        name: "Oliver Emde - Flight Instructor",
-        credentials: "CFI",
-        photo: "assets/images/Instructors/Oliver Emde.jpg",
-        specialties: ["Private", "Single Engine Commercial"],
-        approvedAircraft: ["C172", "PA28"],
-        bio: "Coming from a family of pilots I've always known what I wanted to do with my life. I grew up in Maui, Hawaii, where I embarked on my aviation journey at the age of 17. Flying in Maui with some of the strongest crosswinds in the world I perfected my craft. Since then, I have relocated to California to further pursue my dream. Whether you're pursuing a career or learning for fun, I am eager to share my knowledge to help you become a safe and knowledgeable pilot!",
-        bookingLink: "https://oliveremde11.youcanbook.me/"
-    },
-   
+
     {
         name: "Daniel Durrand - Flight Instructor",
         credentials: "CFI, CFII",
@@ -37,6 +18,7 @@ const instructors = [
         bio: "Let's get you flying! When I started flight training, I was a weekend warrior with an insurance day job, but what started as a fun and challenging hobby became an exciting passion and rewarding career. Regardless of your goals, flying offers you the opportunity to tackle new challenges, embrace adventure, and (literally) see the world from a new perspective. Whether you're looking to get in the plane just for fun, or to eventually fly professionally, I will help you become a safe and capable pilot. I look forward to sharing the cockpit with you!",
         bookingLink: "https://cfidurrand.youcanbook.me/"
     },
+
     {
         name: "Mason Liu - Flight Instructor",
         credentials: "CFI, CFII",
@@ -46,6 +28,17 @@ const instructors = [
         bio: "Flying has always felt like an impossible dream to me. Yet, after earning my PhD in physics and pursuing a career in finance, I decided to start flight training. I discovered where my true passion lies. My academic background and professional experience allow me to connect with both younger learners and those with rich life experience. I would be thrilled to help you achieve your goals and become a safe, capable pilot. Let's take to the skies!",
         bookingLink: "https://masonliu.youcanbook.me/"
     },
+
+    {
+        name: "Oliver Emde - Flight Instructor",
+        credentials: "CFI",
+        photo: "assets/images/Instructors/Oliver Emde.jpg",
+        specialties: ["Private", "Single Engine Commercial"],
+        approvedAircraft: ["C172", "PA28"],
+        bio: "Coming from a family of pilots I've always known what I wanted to do with my life. I grew up in Maui, Hawaii, where I embarked on my aviation journey at the age of 17. Flying in Maui with some of the strongest crosswinds in the world I perfected my craft. Since then, I have relocated to California to further pursue my dream. Whether you're pursuing a career or learning for fun, I am eager to share my knowledge to help you become a safe and knowledgeable pilot!",
+        bookingLink: "https://oliveremde11.youcanbook.me/"
+    },
+    
     {
         name: "Andrew Larson - Flight Instructor",
         credentials: "CFI, CFII",
